e0791ab6f2ac112dd81f7768ddbccc26de3a3c17
itseasy21 <itseasy21@gmail.com>
2024-10-15 22:39:53 +1100

    updated headers

--- src/providers/sources/whvx.ts
+++ src/providers/sources/whvx.ts
@@ -6,17 +6,25 @@ import { NotFoundError } from '@/utils/errors';
 export const baseUrl = 'https://api.whvx.net';
 
 export const headers = {
-  Origin: 'https://www.vidbinge.com',
-  Referer: 'https://www.vidbinge.com',
-  'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-  Accept: 'application/json, text/plain, */*',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Content-Type': 'application/json',
-  Connection: 'keep-alive',
-  Pragma: 'no-cache',
-  'Cache-Control': 'no-cache',
+  ':authority': 'api.whvx.net',
+  ':method': 'GET',
+  ':path': '/status',
+  ':scheme': 'https',
+  accept: '*/*',
+  'accept-encoding': 'gzip, deflate, br, zstd',
+  'accept-language': 'en-US,en;q=0.9',
+  'cache-control': 'no-cache',
+  origin: 'https://www.vidbinge.com',
+  pragma: 'no-cache',
+  priority: 'u=1, i',
+  'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'cross-site',
+  'user-agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
 };
 
 async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {

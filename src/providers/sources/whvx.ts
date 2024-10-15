ac01d0f7b104ed105376aca5702c152298fb71df
itseasy21 <itseasy21@gmail.com>
2024-10-15 22:46:00 +1100

    remove pragma

--- src/providers/sources/whvx.ts
+++ src/providers/sources/whvx.ts
@@ -10,7 +10,6 @@ export const headers = {
   'Accept-Encoding': 'gzip, deflate, br, zstd',
   'Accept-Language': 'en-US,en;q=0.9',
   Origin: 'https://www.vidbinge.com',
-  Pragma: 'no-cache',
   'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
 };

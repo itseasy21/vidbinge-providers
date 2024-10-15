005415c539135c293816402ccdd7eb7f5ab9e358
itseasy21 <itseasy21@gmail.com>
2024-10-15 22:43:51 +1100

    headers update

--- src/providers/sources/whvx.ts
+++ src/providers/sources/whvx.ts
@@ -6,24 +6,12 @@ import { NotFoundError } from '@/utils/errors';
 export const baseUrl = 'https://api.whvx.net';
 
 export const headers = {
-  ':authority': 'api.whvx.net',
-  ':method': 'GET',
-  ':path': '/status',
-  ':scheme': 'https',
-  accept: '*/*',
-  'accept-encoding': 'gzip, deflate, br, zstd',
-  'accept-language': 'en-US,en;q=0.9',
-  'cache-control': 'no-cache',
-  origin: 'https://www.vidbinge.com',
-  pragma: 'no-cache',
-  priority: 'u=1, i',
-  'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"macOS"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'cross-site',
-  'user-agent':
+  Accept: '*/*',
+  'Accept-Encoding': 'gzip, deflate, br, zstd',
+  'Accept-Language': 'en-US,en;q=0.9',
+  Origin: 'https://www.vidbinge.com',
+  Pragma: 'no-cache',
+  'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
 };
 

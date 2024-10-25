9867b124287c005fcecb9a81c3b6b6134ae03711
itseasy21 <itseasy21@gmail.com>
2024-10-26 10:58:00 +1100

    try: whvx test

--- src/providers/sources/whvxMirrors.ts
+++ src/providers/sources/whvxMirrors.ts
@@ -5,8 +5,12 @@ import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 export const baseUrl = 'https://fbox-anywhere-457e019579ad.herokuapp.com/https://mirrors.whvx.net/';
 
 export const headers = {
+  Accept: '*/*',
+  'Accept-Encoding': 'gzip, deflate, br, zstd',
+  'Accept-Language': 'en-US,en;q=0.9',
   Origin: 'https://www.vidbinge.com',
-  Referer: 'https://www.vidbinge.com',
+  'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
 };
 
 async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
